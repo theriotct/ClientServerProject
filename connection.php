3b4c7f469c8ddc665a0b0dff052ee03a900a8b12
Colton Theriot <ctt200911@gmail.com>
2026-04-05 16:52:42 -0500

    fixed connection

--- connection.php
+++ connection.php
@@ -4,8 +4,8 @@ error_reporting(E_ALL);
 
 
 $dbhost = "localhost";
-$dbuser = "root"; //webuser
-$dbpass = ""; //getenv('DATABASE_PASSWORD') for production, hardcoded for testing
+$dbuser = "webuser"; //webuser
+$dbpass = getenv('DATABASE_PASSWORD'); //getenv('DATABASE_PASSWORD') for production, hardcoded for testing
 $dbname = "client_server_db";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)){
