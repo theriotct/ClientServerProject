2fe0ca434f6609d8008bb85ab0fb42984017dac6
Colton Theriot <ctt200911@gmail.com>
2026-04-05 17:30:27 -0500

    updated to bootstrap 5

--- thread.php
+++ thread.php
@@ -45,9 +45,7 @@
     <title>The Power of Awesome Ideas</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
     <!-- Navbar -->
