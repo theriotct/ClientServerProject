950e75688cfe80e4ef843e4a9d04f2f1e83a226c
pgarcia987 <pedrogarcia4900@gmail.com>
2026-04-13 13:16:31 -0500

    Update post.php

--- post.php
+++ post.php
@@ -26,7 +26,7 @@
       
       
       
-  div class="container">
+  <div class="container">
 	  <div class="row">
 	    
 	    <div class="col-md-8 col-md-offset-2">
@@ -34,12 +34,6 @@
     		<h1>Create post</h1>
     		
     		<form action="" method="POST">
-    		    
-    		    <div class="form-group has-error">
-    		        <label for="slug">Slug <span class="require">*</span> <small>(This field use in url path.)</small></label>
-    		        <input type="text" class="form-control" name="slug" />
-    		        <span class="help-block">Field not entered!</span>
-    		    </div>
     		    
     		    <div class="form-group">
     		        <label for="title">Title <span class="require">*</span></label>
@@ -69,5 +63,5 @@
 		
 	</div>
 </div>
-    </body>
+</body>
 </html>
