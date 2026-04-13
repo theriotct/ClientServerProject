d126a53a5650400b9dd36a9f662464ebfc61e74d
pgarcia987 <pedrogarcia4900@gmail.com>
2026-04-13 17:46:35 -0500

    changed the color

--- profile.php
+++ profile.php
@@ -9,7 +9,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <style>
-      body {background-color: #e3f2fd;}
+      body {background-color: lightblue;}
     </style>
   </head>
   <body>
