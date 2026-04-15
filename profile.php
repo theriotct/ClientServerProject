fc4470b2ba896715684501d530c247815bf2e423
Colton Theriot <ctt200911@gmail.com>
2026-04-15 13:29:43 -0500

    Testing dynamic adding data

--- profile.php
+++ profile.php
@@ -7,11 +7,27 @@
   if (!$user_data) {
     not_found();
   }
+  function get_posts($con, $userID){
+    $query = "WITH RECURSIVE thread AS ( SELECT p.postID AS replyID, p.parentID, p.title, p.body, p.authorID, p.date, p.postID AS originalReplyID FROM posts p WHERE p.authorID = $userID UNION ALL SELECT parent.postID, parent.parentID, parent.title, parent.body, parent.authorID, parent.date, t.originalReplyID FROM posts parent JOIN thread t ON t.parentID = parent.postID ) SELECT root.postID AS parentID, root.title AS parentTitle, root.authorID AS parentAuthorID, root.date AS parentDate, reply.postID AS replyID, reply.parentID AS replyParentID, reply.authorID AS replyAuthorID, reply.body AS replyBody, reply.title AS replyTitle, reply.date AS replyDate FROM thread t JOIN posts root ON root.postID = t.postID AND root.parentID IS NULL JOIN posts reply ON reply.postID = t.originalReplyID;";
+    $result = mysqli_query($con, $query);
+
+    if($result){
+        if($result && mysqli_num_rows($result) > 0)
+        {
+            foreach($result as $post){
+                if($post){
+                  echo '<div class="" style="background-color: rgb(234, 234, 234); padding: 10px; border-radius: 5px; margin-top: 10px; margin-bottom: 10px;"><h4><a href="thread.php?postID='.$post['parentID'].'">'.$post['parentTitle'].'</a></h4><p>'.$post['replyBody'].'</p><p>'.$post['replyDate'].'</p></div>';
+                }
+            }
+        }
+    }else{
+        echo 'Error fetching posts';
+    }  
+  }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <title>My Profile</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
