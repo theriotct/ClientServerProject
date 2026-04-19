6c7f0800932cceaf55ac87b3ab00bd18e3e2836b
pgarcia987 <pedrogarcia4900@gmail.com>
2026-04-19 18:09:48 -0500

    Update message.php

--- message.php
+++ message.php
@@ -4,10 +4,55 @@
     <title>Awesome Messages</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css'>
+	<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js'></script>
     <style>
-      body {background-color: #ffe1cb;}
+      body{margin-top:20px;}
+
+		.chat-online {
+    		color: #34ce57
+		}
+
+		.chat-offline {
+    		color: #e4606d
+		}
+
+		.chat-messages {
+    		display: flex;
+    		flex-direction: column;
+    		max-height: 800px;
+    		overflow-y: scroll
+		}
+
+		.chat-message-left,
+		.chat-message-right {
+    		display: flex;
+    		flex-shrink: 0
+		}
+
+		.chat-message-left {
+    		margin-right: auto
+		}
+
+		.chat-message-right {
+    		flex-direction: row-reverse;
+    		margin-left: auto
+		}
+		.py-3 {
+    		padding-top: 1rem!important;
+    		padding-bottom: 1rem!important;
+		}
+		.px-4 {
+    		padding-right: 1.5rem!important;
+    		padding-left: 1.5rem!important;
+		}
+		.flex-grow-0 {
+    		flex-grow: 0!important;
+		}
+		.border-top {
+    		border-top: 1px solid #dee2e6!important;
+		}
     </style>
   </head>
   <body>
