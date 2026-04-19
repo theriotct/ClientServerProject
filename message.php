39fd8102eb138f0c4cadfcc38b3f0b88cb6a4a71
pgarcia987 <pedrogarcia4900@gmail.com>
2026-04-19 17:51:01 -0500

    Update message.php

--- message.php
+++ message.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
     <style>
-      body {background-color: lightblue;}
+      
     </style>
   </head>
   <body>
