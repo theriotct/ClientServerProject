d1942af31b8b430e3b502a9f4c0c62ae8559b4d2
pgarcia987 <pedrogarcia4900@gmail.com>
2026-04-19 17:53:30 -0500

    Update message.php

--- message.php
+++ message.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
     <style>
       
     </style>
