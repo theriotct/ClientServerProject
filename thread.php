3d6af3b3c089f98d62f84ec34b3c062550b9e381
Colton Theriot <ctt200911@gmail.com>
2026-04-22 13:34:24 -0500

    added a hidden postID

--- thread.php
+++ thread.php
@@ -60,6 +60,12 @@
                 alert('Error posting reply');
             }
         }
+        if(isset($_POST['like'])){
+
+        }
+        if(isset($_POST['dislike'])){
+
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -135,11 +141,14 @@
         </p>
       </div>
       <div class="panel-footer">
-        <a href="#" class="btn btn-sm btn-default">Like: <?php echo $posts[0]['totalLikes']?></a>
-        <a href="#" class="btn btn-sm btn-default">Dislike: <?php echo $posts[0]['totalDislikes']?></a>
-        <a href="#" class="btn btn-sm btn-default">Report</a>
-        <a href="#" class="btn btn-sm btn-default btn-danger right">Delete</a>
-        <a href="#" class="btn btn-sm btn-default right">Edit</a>
+        <form action="" method="POST">
+          <input type="text" value="<?php echo $posts[0]['postID']?>" name="postID" hidden>
+          <input class="btn btn-sm btn-default" type="submit" name="like" value="Like: '.$posts[$i]['totalLikes'].'">
+          <input class="btn btn-sm btn-default" type="submit" name="dislike" value="Dislike: '.$posts[$i]['totalDislikes'].'">
+          <input class="btn btn-sm btn-default" type="submit" name="dislike" value="Report">
+          <input class="btn btn-sm btn-default btn-danger right" type="submit" name="dislike" value="Delete">
+          <input class="btn btn-sm btn-default right" type="submit" name="dislike" value="Edit">
+        </form>
       </div>
     </div>
 
