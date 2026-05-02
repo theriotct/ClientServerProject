e5c92536242ec5e5f83533be037876e8fa8f180c
pgarcia987 <pedrogarcia4900@gmail.com>
2026-05-02 14:42:30 -0500

    got rid of the user titles and the status section

--- admin/manageUsers.php
+++ admin/manageUsers.php
@@ -212,7 +212,7 @@
 						<tr>
 							<th><span>User</span></th>
 							<th><span>Created</span></th>
-							<th class="text-center"><span>Status</span></th>
+							
 							<th><span>Email</span></th>
 							<th>&nbsp;</th>
 						</tr>
@@ -222,14 +222,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
 								<a href="#" class="user-link">Mila Kunis</a>
-								<span class="user-subhead">Admin</span>
+								
 							</td>
 							<td>
 								2013/08/08
 							</td>
-							<td class="text-center">
-								<span class="label label-default">Inactive</span>
-							</td>
+							
 							<td>
 								<a href="#">mila@kunis.com</a>
 							</td>
@@ -258,14 +256,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="">
 								<a href="#" class="user-link">George Clooney</a>
-								<span class="user-subhead">Member</span>
+								
 							</td>
 							<td>
 								2013/08/12
 							</td>
-							<td class="text-center">
-								<span class="label label-success">Active</span>
-							</td>
+							
 							<td>
 								<a href="#">marlon@brando.com</a>
 							</td>
@@ -294,14 +290,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="">
 								<a href="#" class="user-link">Ryan Gossling</a>
-								<span class="user-subhead">Registered</span>
+								
 							</td>
 							<td>
 								2013/03/03
 							</td>
-							<td class="text-center">
-								<span class="label label-danger">Banned</span>
-							</td>
+							
 							<td>
 								<a href="#">jack@nicholson</a>
 							</td>
@@ -330,14 +324,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar4.png" alt="">
 								<a href="#" class="user-link">Emma Watson</a>
-								<span class="user-subhead">Registered</span>
+								
 							</td>
 							<td>
 								2004/01/24
 							</td>
-							<td class="text-center">
-								<span class="label label-warning">Pending</span>
-							</td>
+							
 							<td>
 								<a href="#">humphrey@bogart.com</a>
 							</td>
@@ -366,14 +358,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar5.png" alt="">
 								<a href="#" class="user-link">Robert Downey Jr.</a>
-								<span class="user-subhead">Admin</span>
+								
 							</td>
 							<td>
 								2013/12/31
 							</td>
-							<td class="text-center">
-								<span class="label label-success">Active</span>
-							</td>
+							
 							<td>
 								<a href="#">spencer@tracy</a>
 							</td>
@@ -402,14 +392,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="">
 								<a href="#" class="user-link">Mila Kunis</a>
-								<span class="user-subhead">Admin</span>
+								
 							</td>
 							<td>
 								2013/08/08
 							</td>
-							<td class="text-center">
-								<span class="label label-default">Inactive</span>
-							</td>
+							
 							<td>
 								<a href="#">mila@kunis.com</a>
 							</td>
@@ -438,14 +426,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="">
 								<a href="#" class="user-link">George Clooney</a>
-								<span class="user-subhead">Member</span>
+								
 							</td>
 							<td>
 								2013/08/12
 							</td>
-							<td class="text-center">
-								<span class="label label-success">Active</span>
-							</td>
+							
 							<td>
 								<a href="#">marlon@brando.com</a>
 							</td>
@@ -474,14 +460,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
 								<a href="#" class="user-link">Ryan Gossling</a>
-								<span class="user-subhead">Registered</span>
+								
 							</td>
 							<td>
 								2013/03/03
 							</td>
-							<td class="text-center">
-								<span class="label label-danger">Banned</span>
-							</td>
+							
 							<td>
 								<a href="#">jack@nicholson</a>
 							</td>
@@ -510,14 +494,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
 								<a href="#" class="user-link">Emma Watson</a>
-								<span class="user-subhead">Registered</span>
+								
 							</td>
 							<td>
 								2004/01/24
 							</td>
-							<td class="text-center">
-								<span class="label label-warning">Pending</span>
-							</td>
+							
 							<td>
 								<a href="#">humphrey@bogart.com</a>
 							</td>
@@ -546,14 +528,12 @@
 							<td>
 								<img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="">
 								<a href="#" class="user-link">Robert Downey Jr.</a>
-								<span class="user-subhead">Admin</span>
+								
 							</td>
 							<td>
 								2013/12/31
 							</td>
-							<td class="text-center">
-								<span class="label label-success">Active</span>
-							</td>
+							
 							<td>
 								<a href="#">spencer@tracy</a>
 							</td>
