35b0414df977c1ec380bb02241211bae475f5c27
Colton Theriot <ctt200911@gmail.com>
2026-05-02 13:55:44 -0500

    fixed parentheses

--- thread.php
+++ thread.php
@@ -239,7 +239,7 @@
                     <input class="btn btn-sm btn-default" type="submit" name="like" value="Like: '.$posts[$i]['totalLikes'].'" '.(!$user_data ? ' disabled' : '').'>
                     <input class="btn btn-sm btn-default" type="submit" name="dislike" value="Dislike: '.$posts[$i]['totalDislikes'].'" '.(!$user_data ? ' disabled' : '').'>
                     <input class="btn btn-sm btn-default" type="submit" name="dislike" value="Report" '.(!$user_data ? ' disabled' : '').'>';
-                    if($user_data && ($user_data['userID'] == $posts[$i]['authorID'] ) || !is_null($user_data['isAdmin'])){
+                    if($user_data && ($user_data['userID'] == $posts[$i]['authorID']  || !is_null($user_data['isAdmin']))){
                       echo '<input class="btn btn-sm btn-default btn-danger right" type="submit" name="dislike" value="Delete">
                             <input class="btn btn-sm btn-default right" type="submit" name="dislike" value="Edit">';
                     }
