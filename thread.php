dc80c50245c379304ff16040675527ccc8e6c6a0
Colton Theriot <ctt200911@gmail.com>
2026-05-02 14:19:26 -0500

    fixed button names

--- thread.php
+++ thread.php
@@ -246,10 +246,10 @@
                     <input type="text" value="'.$posts[$i]['postID'].'" name="postID" hidden>
                     <input class="btn btn-sm btn-default" type="submit" name="like" value="Like: '.$posts[$i]['totalLikes'].'" '.(!$user_data ? ' disabled' : '').'>
                     <input class="btn btn-sm btn-default" type="submit" name="dislike" value="Dislike: '.$posts[$i]['totalDislikes'].'" '.(!$user_data ? ' disabled' : '').'>
-                    <input class="btn btn-sm btn-default" type="submit" name="dislike" value="Report" '.(!$user_data ? ' disabled' : '').'>';
+                    <input class="btn btn-sm btn-default" type="submit" name="report" value="Report" '.(!$user_data ? ' disabled' : '').'>';
                     if($user_data && ($user_data['userID'] == $posts[$i]['authorID']  || !is_null($user_data['isAdmin']))){
-                      echo '<input class="btn btn-sm btn-default btn-danger right" type="submit" name="dislike" value="Delete">
-                            <input class="btn btn-sm btn-default right" type="submit" name="dislike" value="Edit">';
+                      echo '<input class="btn btn-sm btn-default btn-danger right" type="submit" name="delete" value="Delete">
+                            <input class="btn btn-sm btn-default right" type="submit" name="edit" value="Edit">';
                     }
                     echo '
                   </form>
