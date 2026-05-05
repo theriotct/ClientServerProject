fa5bcf966b2e8c9b547d22aafc7fa2cd5cfd4c03
Colton Theriot <ctt200911@gmail.com>
2026-05-05 14:10:27 -0500

    changed more on image php

--- image.php
+++ image.php
@@ -3,7 +3,7 @@ include('connection.php');
 
 $id = (int)($_GET['id'] ?? 0);
 
-$query = "SELECT ImageData FROM marketplace_items WHERE itemID = ?";
+$query = "SELECT imageData FROM marketplace_items WHERE itemID = ?";
 $stmt = mysqli_prepare($con, $query);
 mysqli_stmt_bind_param($stmt, 'i', $id);
 mysqli_stmt_execute($stmt);
@@ -11,16 +11,24 @@ $result = mysqli_stmt_get_result($stmt);
 
 if ($row = mysqli_fetch_assoc($result)) {
 
-    $img = $row['ImageData'];
+    $image = $row['imageData'];
 
-    if (!empty($img)) {
-        // Try to detect image type
+    if ($image !== null) {
+
+        // Try to detect image type (basic but useful)
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
-        $mime = finfo_buffer($finfo, $img);
+        $mime = finfo_buffer($finfo, $image);
         finfo_close($finfo);
 
         header("Content-Type: " . $mime);
-        echo $img;
+        header("Content-Length: " . strlen($image));
+
+        echo $image;
+        exit;
     }
 }
+
+// fallback image (optional)
+http_response_code(404);
+echo "Image not found";
 ?>
