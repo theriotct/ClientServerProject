1e4dab9a31a348fde80508ff1ac8bc679a984682
Colton Theriot <ctt200911@gmail.com>
2026-05-05 15:22:18 -0500

    Adjusted form

--- admin/manageAdmins.php
+++ admin/manageAdmins.php
@@ -255,7 +255,7 @@
 									<form action="manageAdmins.php" method="POST" style="display:inline;">
 										<input type="hidden" name="userID" value="<?= $user['userID'] ?>">
 
-										<button type="submit" name="deleteUser" class="table-link danger" style="background:none; border:none; padding:0;">
+										<button type="submit" name="deleteUser" class="table-link danger btn-icon">
 											<span class="fa-stack">
 												<i class="fa fa-square fa-stack-2x"></i>
 												<i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
