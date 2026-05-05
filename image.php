410331d2b7b3099331cef20ec97ee821ff08a671
Colton Theriot <ctt200911@gmail.com>
2026-05-05 13:59:30 -0500

    made image.php send back all types of images

--- image.php
+++ image.php
@@ -10,7 +10,7 @@ mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 
 if ($row = mysqli_fetch_assoc($result)) {
-    header("Content-Type: image/jpeg"); // you can improve this later
+    header("Content-Type: image/*"); // you can improve this later
     echo $row['ImageData'];
 }
 ?>
