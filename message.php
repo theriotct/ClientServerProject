bd726a0b73d0306d05b87996cf28e53114d5433a
Colton Theriot <ctt200911@gmail.com>
2026-05-05 23:31:16 -0500

    updated messages for custom user profile images

--- message.php
+++ message.php
@@ -188,7 +188,7 @@
 						    <div class="d-flex align-items-center py-1">
 								<?php if (isset($contact_data)) : ?>
 							    <div class="position-relative">
-								    <img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="rounded-circle mr-1" alt="<?php echo htmlspecialchars($contact_data['username']); ?>" width="40" height="40">
+								    <img src="image.php?userID=' . (int)$contact_data['userID'] . '" class="rounded-circle mr-1" alt="<?php echo htmlspecialchars($contact_data['username']); ?>" width="40" height="40">
 							    </div>
 							    <div class="flex-grow-1 pl-3">
 								    <strong><?php echo htmlspecialchars($contact_data['username']); ?></strong>
@@ -239,7 +239,7 @@
 												    // Message sent by the logged-in user
 													echo '<div class="chat-message-right mb-4">
 														<div>
-															<img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="rounded-circle mr-1" alt="'.$user_data['username'].'" width="40" height="40">
+															<img src="image.php?userID=' . (int)$user_data['userID'] . '" class="rounded-circle mr-1" alt="'.$user_data['username'].'" width="40" height="40">
 															<div class="text-muted small text-nowrap mt-2">'.$row['date'].'</div>
 														</div>
 														<div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
@@ -252,7 +252,7 @@
 												    // Message sent by the contact
 												    echo '<div class="chat-message-left pb-4">
 														<div>
-															<img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="rounded-circle mr-1" alt="'.$contact_data['username'].'" width="40" height="40">
+															<img src="image.php?userID=' . (int)$contact_data['userID'] . '" class="rounded-circle mr-1" alt="'.$contact_data['username'].'" width="40" height="40">
 															<div class="text-muted small text-nowrap mt-2">'.$row['date'].'</div>
 														</div>
 														<div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
