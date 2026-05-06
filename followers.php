d68dcdcde2c0794c0d50dfdbe8bb9bee4771a082
pgarcia987 <pedrogarcia4900@gmail.com>
2026-05-05 21:21:14 -0500

    added tab name

--- followers.php
+++ followers.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Manage Admins</title>
+    <title>Awesome Followers</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
